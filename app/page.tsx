036197e40bb40bc21d1c275fd046b9543e6bd254
Joel <ajiolajoel@gmail.com>
2023-09-28 14:37:34 +0100

    next-js flexible project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return(
+    <section className="flex-start flex-col paddings mb-16">
+        <h1>Category</h1>
+        <h1>Posts</h1>
+        <h1>Loadore</h1>
+      </section>
+  )
+}
+
+export default Home
