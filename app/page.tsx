9a1ed804a35db6846ad3f035fd902d9c8d08f161
Joel <ajiolajoel@gmail.com>
2023-09-28 17:36:41 +0100

    update to flexible-project

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="flex-start flex-col paddings mb-16">
         <h1>Category</h1>
         <h1>Posts</h1>
-        <h1>Loadore</h1>
+        <h1>LoadMore</h1>
       </section>
   )
 }
